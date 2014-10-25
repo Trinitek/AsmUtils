dcca232472e088d25a33df4063edb2ea9f680edd
Trinitek <trinitek@live.com>
2014-10-25 12:28:31 -0400

    Add manifest, modify output directory structure

--- 16color256index/src/Main.java
+++ 16color256index/src/Main.java
@@ -1,6 +1,16 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.out.println("Wrong number of arguments");
+            return;
+        }
+
+        BufferedImage sourceImage = ImageIO.read(new File(args[0]));
     }
 }
